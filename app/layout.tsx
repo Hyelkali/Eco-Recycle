fa07443cda6ac917f43cc14f57e006b0a09ab26c
HyelKali <hyelnamuninathan@gmail.com>
2025-09-24 16:53:27 +0100

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,11 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'EcoRecycle',
+  description: 'Created by Hyelkali',
+  icons:{
+      icon: '/logo.png'
+  }
 }
 
 export default function RootLayout({
